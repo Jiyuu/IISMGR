f3236ad372bbf9ab12af3246a43f7c912885cca1
Jiyuu <Jiyuu@github.com>
2013-11-16 01:46:50 +0200

    init

---
+++ IIsManage/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.Administration;
+
+namespace IIsManage
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Web.Administration.ServerManager sm = new ServerManager();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var site in sm.Sites)
+            {
+                site.
+            }
+        }
+    }
+}
